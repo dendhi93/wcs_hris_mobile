fb37f49582ace9af19bcffa631d09b193ef9c364
dendhi93 <dendhi.fendi@gmail.com>
2019-12-16 15:39:59 +0700

    enhanchment in custom approval adapter

--- app/src/main/java/com/wcs/mobilehris/feature/approval/CustomApprovalAdapter.kt
+++ app/src/main/java/com/wcs/mobilehris/feature/approval/CustomApprovalAdapter.kt
@@ -15,15 +15,15 @@ import com.wcs.mobilehris.utils.MessageUtils
 class CustomApprovalAdapter (private val _context : Context, private val approvalList : MutableList<ApprovalModel>):
     RecyclerView.Adapter<CustomApprovalAdapter.ViewHolder>(){
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomApprovalAdapter.ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.custom_list_menu,parent,false))
     }
 
     override fun getItemCount(): Int = approvalList.size
 
-    override fun onBindViewHolder(holder: CustomApprovalAdapter.ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         var model : ApprovalModel = approvalList[position]
-        val stMenu = model.itemMenu.trim()+" "+ model.qtyApproval.toString()
+        val stMenu = model.itemMenu.trim()+" ("+ model.qtyApproval.toString() +")"
         holder.tvMenu.text = stMenu.trim()
         holder.tvMenuContent.text = model.itemMenuContent.trim()
         holder.imgCustom.setImageResource(model.imgItemMenu)
@@ -38,7 +38,5 @@ class CustomApprovalAdapter (private val _context : Context, private val approva
         var imgCustom : ImageView = view.findViewById(R.id.imgV_custom)
         var tvMenuContent : TextView = view.findViewById(R.id.tv_custom_content)
     }
-
-
 
 }
