8b842bcbe025c12b6e3122b86391e55dfc719f4c
dendhi93 <dendhi.fendi@gmail.com>
2020-03-26 10:05:37 +0700

    1. redesign team fragment 2. add api team in team fragment

--- app/src/main/java/com/wcs/mobilehris/feature/team/fragment/TeamViewModel.kt
+++ app/src/main/java/com/wcs/mobilehris/feature/team/fragment/TeamViewModel.kt
@@ -1,62 +1,73 @@
 package com.wcs.mobilehris.feature.team.fragment
 
 import android.content.Context
-import android.os.Handler
 import androidx.databinding.ObservableField
 import androidx.lifecycle.ViewModel
 import com.wcs.mobilehris.R
+import com.wcs.mobilehris.connection.ApiRepo
 import com.wcs.mobilehris.connection.ConnectionObject
 import com.wcs.mobilehris.util.ConstantObject
+import com.wcs.mobilehris.util.DateTimeUtils
+import org.json.JSONArray
+import org.json.JSONObject
 
-class TeamViewModel (private val context : Context, private val teamInterface: TeamInterface) : ViewModel(){
+class TeamViewModel (private val context : Context,
+                     private val teamInterface: TeamInterface,
+                     private val apiRepo: ApiRepo) : ViewModel(){
     val isHiddenSearch = ObservableField(false)
 
-    fun initDataTeam(typeOfLoading : Int){
+    fun initDataTeam(teamName : String){
         when{
             !ConnectionObject.isNetworkAvailable(context) -> teamInterface.onAlertTeam(context.getString(R.string.alert_no_connection),
                     ConstantObject.vAlertDialogNoConnection, TeamFragment.ALERT_TEAM_NO_CONNECTION)
-            else -> getTeamData(typeOfLoading)
+            else -> getTeamData(teamName)
         }
     }
 
-    private fun getTeamData(typeLoading : Int){
+    private fun getTeamData(teamName : String){
         teamInterface.hideUI(ConstantObject.vRecylerViewUI)
         teamInterface.showUI(ConstantObject.vGlobalUI)
+        teamInterface.showUI(ConstantObject.vProgresBarUI)
         val listTeam = mutableListOf<TeamModel>()
-        var teamModel = TeamModel(
-            "62909090", "Andika Kurnia",
-            "+620878945445",
-            "andika.kurnia@id.wilmar-intl.com"
-        )
-        listTeam.add(teamModel)
-        teamModel = TeamModel(
-            "62909091", "Claudia",
-            "+620878945489",
-            "claudia@id.wilmar-intl.com"
-        )
-        listTeam.add(teamModel)
-        teamModel = TeamModel(
-            "62909092", "Michael Saputra",
-            "+62087894547878",
-            "michael.saputra@id.wilmar-intl.com"
-        )
-        listTeam.add(teamModel)
+        var teamModel : TeamModel
+        apiRepo.searchDataTeam(teamName, DateTimeUtils.getCurrentDate(), DateTimeUtils.getCurrentDate(), context, object : ApiRepo.ApiCallback<JSONObject>{
+            override fun onDataLoaded(data: JSONObject?) {
+                data?.let {
+                    val responseSearchTeam = it.getString(ConstantObject.vResponseData)
+                    val jArrayTeam = JSONArray(responseSearchTeam)
+                    if(jArrayTeam.length() > 0){
+                        for(i in 0 until jArrayTeam.length()){
+                            val jObjTeam = jArrayTeam.getJSONObject(i)
 
-        when{
-            listTeam.size > 0 -> {
-                Handler().postDelayed({
-                    teamInterface.onLoadTeam(listTeam, typeLoading)
-                }, 2000)
+                            teamModel = TeamModel(
+                                jObjTeam.getString("NIK"),
+                                jObjTeam.getString("FULL_NAME"),
+                                jObjTeam.getString("PHONE_NUMBER"),
+                                jObjTeam.getString("EMAIL")
+
+                            )
+                            listTeam.add(teamModel)
+                        }
+                    }
+
+                    teamInterface.onClearTeamList()
+                    when(listTeam.size){
+                        0 -> {
+                        teamInterface.showUI(ConstantObject.vGlobalUI)
+                        teamInterface.hideUI(ConstantObject.vRecylerViewUI)
+                        teamInterface.hideUI(ConstantObject.vProgresBarUI)
+                        }
+                        else -> teamInterface.onLoadTeam(listTeam)
+                    }
+                }
             }
-            else -> {
+
+            override fun onDataError(error: String?) {
+                teamInterface.onClearTeamList()
                 teamInterface.showUI(ConstantObject.vGlobalUI)
                 teamInterface.hideUI(ConstantObject.vRecylerViewUI)
-                when(typeLoading){
-                    ConstantObject.vLoadWithProgressBar -> teamInterface.hideUI(ConstantObject.vProgresBarUI)
-                    else -> teamInterface.onHideSwipeRefresh()
-                }
-                teamInterface.onErrorMessage(context.getString(R.string.no_data_found), ConstantObject.vToastInfo)
+                teamInterface.hideUI(ConstantObject.vProgresBarUI)
             }
-        }
+        })
     }
 }
